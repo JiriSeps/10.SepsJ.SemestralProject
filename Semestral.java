9b888f87eb579e519e34c0d77e4d7fa8786a2904
JiriSeps <115661301+JiriSeps@users.noreply.github.com>
2022-12-20 20:35:43 +0100

    Add files via upload

---
+++ Semestral.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TEN.SepsJ.SemestralProject;
+
+import java.util.Scanner;
+
+/**
+ *10. Program converts an input number into the count of individual numbers
+ * within the input number.
+ * 
+ * @author fiwie
+ * @version 1.0 17/12/2022
+ */
+public class Semestral {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //in
+        System.out.println("Zadej cislo: ");
+        long num = sc.nextLong();
+        
+        while (num > 0) {
+        // Count the number of occurrences of each digit
+        int[] counts = countDigits(num);
+
+        // Print the count and digit for each non-zero count
+        String result = "";
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] > 0) {
+            result += counts[i] + "" + i;
+            }
+        }
+
+        // Compare the input number and result
+        System.out.println("Cislo "+num+" ma soupisku "+result);
+        if (result.equals(String.valueOf(num))) {
+            System.out.println("Cislo"+num+" je zapisem sve soupisky");
+        } else {
+            System.out.println("Cislo "+num+" neni zapisem sve soupisky");
+        }
+
+        System.out.print("Zadej cislo: ");
+        num = sc.nextInt();
+        }
+    }
+
+    public static int[] countDigits(long num) {
+        int[] counts = new int[10]; // array creation for digit storage
+        
+        while (num > 0) {
+        int digit = (int) (num % 10);
+        counts[digit]++;
+        num /= 10;
+        }
+
+        return counts;
+    }
+}
