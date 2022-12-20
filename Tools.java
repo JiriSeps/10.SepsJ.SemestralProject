e10ca2864161dd9c4c1df9863bcc79cba285bb22
JiriSeps <115661301+JiriSeps@users.noreply.github.com>
2022-12-20 23:15:17 +0100

    Update Tools.java

--- Tools.java
+++ Tools.java
@@ -9,6 +9,25 @@ package TEN.SepsJ.SemestralProject;
  * @author fiwie
  */
 public class Tools {
+    public static String toString(int[] counts) {
+        String result = "";
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] > 0) {
+                result += counts[i] + "" + i;
+            }
+        }
+        return result;
+    }
+
+    public static void compare(long num, String result) {
+        System.out.println("Cislo " + num + " ma soupisku " + result);
+        if (result.equals(String.valueOf(num))) {
+            System.out.println("Cislo" + num + " je zapisem sve soupisky");
+        } else {
+            System.out.println("Cislo " + num + " neni zapisem sve soupisky");
+        }
+
+    }
     public static int[] countDigits(long num) {
         int[] counts = new int[10]; // array creation for digit storage
         
